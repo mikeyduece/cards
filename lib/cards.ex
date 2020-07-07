42acefa43584e4249b1b1efd709d6420d29cc880
Mike Heft <mikeheft@gmail.com>
2020-07-07 07:43:10 -0600

    Add load function with error handling

--- lib/cards.ex
+++ lib/cards.ex
@@ -4,12 +4,14 @@ defmodule Cards do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
+    # Use in place of nested iteration. Flatmaps the two arrays
     for suit <- suits, value <- values do
       "#{value} of #{suit}"
     end
   end
 
   def deal(deck, hand_size) do
+    # Splits deck into as many elements as the hand_size
     Enum.split(deck, hand_size)
   end
 
@@ -18,7 +20,24 @@ defmodule Cards do
   end
 
   def contains?(deck, card) do
+    # Similar to Ruby' #include?
     Enum.member?(deck, card)
+  end
+
+  def save(deck, filename) do
+    # Invokes erlang language
+    binary = :erlang.term_to_binary(deck)
+    # Write deck to filesystem
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    { status, binary } = Files.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "File does not exist"
+    end
   end
 
 end
